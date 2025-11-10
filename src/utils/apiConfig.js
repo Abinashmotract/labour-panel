8264c3471d18567f42bc69918d378484993f139b
Abinash Sinha <sinhaabinash47>
2025-11-10 16:53:28 +0530

    update url

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://labour-backend-1.onrender.com/api";
+export const API_BASE_URL = "https://nearbylabour.com/api";
 // export const API_BASE_URL = "http://localhost:3512/api";
